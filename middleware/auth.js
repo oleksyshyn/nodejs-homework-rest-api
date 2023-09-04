6dc5a156337e80bcdc53cef19d998318d38dda33
Iryna Oleksyshyn <irynkaoleksyshyn@gmail.com>
2023-09-04 10:17:58 +0300

    add token validation

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,7 +11,7 @@ async function auth(req, res, next) {
 
   const [bearer, token] = authHeader.split(" ", 2);
 
-  if (bearer !== "Bearer") {
+  if (bearer !== "Bearer" || !token) {
     return res.status(401).json({ message: "Not authorized" });
   }
 
